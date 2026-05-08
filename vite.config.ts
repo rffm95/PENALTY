63277cb7a6cdbbd9cee0254a894e235828e5da9d
rffm95 <sucessomacico@gmail.com>
2026-05-08 07:48:56 +0100

    Change base path in vite.config.ts
    Update base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/PENALTY/',          // isto é essencial para GitHub Pages subpath
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     host: '0.0.0.0',
     allowedHosts: true,
   },
-});
+})
